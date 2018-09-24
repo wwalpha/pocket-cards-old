435739f460a8a4b5784ddc0716bc24e7d758d1b1
wwalpha <wwalpha@gmail.com>
2018-09-24 16:02:38 +0900

    update cdk

--- cdk/src/lambda/index.ts
+++ cdk/src/lambda/index.ts
@@ -1,2 +1,23 @@
+import { CommonProps } from '../common';
+
 export { default as ImageToWord } from './image_to_word';
 export { default as AddNewWords } from './add-new-words';
+
+export const getHandler = (props: LambdaInput, functionName: string, handler: string): string => {
+  if (props.envType === 'dev') {
+    return `${functionName}/app.handler`;
+  }
+
+  return handler;
+};
+
+
+export interface LambdaInput extends CommonProps {
+  bucketName: string,
+}
+
+export interface LambdaOutput {
+  // Lambda Arns
+  [key: string]: string;
+}
+

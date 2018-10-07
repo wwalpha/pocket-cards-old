0c1e5a903d7243e3755af6f1ae9437c0e183af08
wwalpha <wwalpha@gmail.com>
2018-10-07 17:48:42 +0900

    update folder

--- lambda/src/utils/commons/utils.ts
+++
@@ -1,4 +0,0 @@
-
-export const defaultEmpty = (value: string | undefined = ''): string => {
-  return value;
-};
--- lambda/typings/external.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference path="../node_modules/aws-sdk/index.d.ts" />
-/// <reference path="../node_modules/moment/moment.d.ts" />
